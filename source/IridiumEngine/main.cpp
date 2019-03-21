e97965e63e7d54ac0cc01991b2d553b415eff6e8
Diego Camacho <DiegoDC97@gmail.com>
2019-03-20 23:04:56 -0400

    Engine executable split from engine library

--- source/IridiumEngine/main.cpp
+++ source/IridiumEngine/main.cpp
@@ -1,11 +1,11 @@
-#include "Engine/Engine.h"
-#include <iostream>
-
-
-int main()
-{
-	if (IridiumEngine::Instance()->Initialize())
-		IridiumEngine::Instance()->Run();
-
-	return 0;
-}
+//#include "Engine/Engine.h"
+//#include <iostream>
+//
+//
+//int main()
+//{
+//	if (IridiumEngine::Instance()->Initialize())
+//		IridiumEngine::Instance()->Run();
+//
+//	return 0;
+//}
